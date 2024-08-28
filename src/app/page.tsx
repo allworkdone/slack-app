79d91e45ba0b8d8cba57a77092afcb8e6099af1e
allworkdone <raj635321@gmail.com>
2024-08-28 10:56:15 +0530

    slack-app: Implimented Auth Screen shadcn-ui: added seperator, input, card react-icons: added react-icons for icon css: applied global css property for better layout sign-in | sign-up : designed the card layout added google and github login methods assigned state changing method for sign-up | sign-in implimented event handler for sign-in and sign-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { AuthScreen } from "@/features/auth/components/auth-screen";
+
 export default function Home() {
   return (
-    <div className="text-rose-500 font-bold text-2xl">
-      Hello world
-    </div>
+    <AuthScreen/>
+
   );
 }
